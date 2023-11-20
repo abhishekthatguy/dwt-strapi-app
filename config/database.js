86ce620b743d4ebcb34769d9b1417c902b1574e4
Abhishek <kashyapabhi688@gmail.com>
2023-11-21 02:32:43 +0530

    changed host name again to fix issue

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'mysql-3huo'),
+        host: env('DATABASE_HOST', '44.226.145.213'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'mysql'),
         username: env('DATABASE_USERNAME', '1bSmTSmhHUwD4lAPROHIbOKPKHYUfCdIXIJmfNoBOIg='),
