677fac00a333457c571fefd50b1ae3be6e00607a
Abhishek <kashyapabhi688@gmail.com>
2023-11-21 02:02:32 +0530

    changed database configuration for deployment

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'dwt-strapi-app'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'abhishekR00t!'),
+        database: env('DATABASE_NAME', 'mysql'),
+        username: env('DATABASE_USERNAME', '1bSmTSmhHUwD4lAPROHIbOKPKHYUfCdIXIJmfNoBOIg='),
+        password: env('DATABASE_PASSWORD', 'mysql'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
